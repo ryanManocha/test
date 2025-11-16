f0b3a88bfe91ce6f7c82b0e3fd060eb295dee5ef
ryanM154 <55896400+ryanManocha@users.noreply.github.com>
2025-11-15 16:32:29 -0800

    Update Painter.java

--- Painter.java
+++ Painter.java
@@ -2,193 +2,186 @@ package stacklab;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
-
+import java.util.Stack;
 import javax.swing.*;
 import javax.swing.event.*;
 
+public class Painter extends JFrame implements ActionListener {
 
-public class Painter extends JFrame implements ActionListener
-{
-	private Painter				outerThis;
-	private JButton				undoBtn;
-	private JButton				redoBtn;
-	private JButton				quitBtn;
-	private ColorChooser		colorChooser;
-	private Canvas				canvas;
-	private Stack<Circle> 		history;
-	private Stack<Circle> 		undoHistory;
-	
-	
-	private class ColorChooser extends JPanel
-	{
-		JSlider[]		sliders;
-		SamplerPanel	sampler;
-		
-		ColorChooser()
-		{
-			setLayout(new BorderLayout());
-			sampler = new SamplerPanel();
-			
-			JPanel west = new JPanel(new GridLayout(3, 1));
-			sliders = new JSlider[3];
-			for (int i=0; i<3; i++)
-			{
-				JPanel p = new JPanel();
-				p.add(new JLabel("" + "RGB".charAt(i)));
-				sliders[i] = new JSlider(0, 255, (i==0) ? 255 : 0);
-				sliders[i].addChangeListener(sampler);
-				p.add(sliders[i]);
-				west.add(p);
-			}
-			add(west, BorderLayout.WEST);
-			add(sampler, BorderLayout.CENTER);
-		}
-		
-		Color getColor()
-		{
-			int r = sliders[0].getValue();
-			int g = sliders[1].getValue();
-			int b = sliders[2].getValue();
-			return new Color(r, g, b);
-		}
-		
-		private class SamplerPanel extends JPanel implements ChangeListener
-		{
-			public Dimension getPreferredSize()
-			{
-				return new Dimension(50, 50);
-			}
-			
-			public void stateChanged(ChangeEvent e) 
-			{
-				repaint();
-			}
-			
-			public void paintComponent(Graphics g)
-			{
-				g.setColor(getColor());
-				g.fillRect(0, 0, getWidth(), getHeight());
-			}
-		}
-	}
-	
-	
-	private class Canvas extends JPanel implements MouseListener
-	{
-		Canvas()
-		{
-			addMouseListener(this);
-		}
-		
-		public Dimension getPreferredSize()
-		{
-			return new Dimension(500, 500);
-		}
+    private JButton undoBtn;
+    private JButton redoBtn;
+    private JButton quitBtn;
+    private ColorChooser colorChooser;
+    private Canvas canvas;
 
-		public void mouseClicked(MouseEvent e) 
-		{
-			Circle circle = new Circle(e.getPoint(), 50, colorChooser.getColor());
-			history.push(circle);
-			repaint();
-			undoBtn.setEnabled(true);
-		}
+    private Stack<Circle> history;
+    private Stack<Circle> undoHistory;
 
-		public void mousePressed(MouseEvent e) { }
-		public void mouseReleased(MouseEvent e) { }
-		public void mouseEntered(MouseEvent e) { }
-		public void mouseExited(MouseEvent e) { }
-		
-		public void paintComponent(Graphics g)
-		{
-			g.setColor(Color.WHITE);
-			g.fillRect(0, 0, getWidth(), getHeight());
-			for (Circle circle: history)
-				circle.paint(g);
-		}
-	}  // End of inner class Canvas
-	
-	
-	Painter()
-	{
-		outerThis = this;
-		
-		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		history = new Stack<>();
-		undoHistory = new Stack<>();
-		
-		JPanel pan = new JPanel();
-		colorChooser = new ColorChooser();
-		pan.add(colorChooser);
-		undoBtn = new JButton("Undo");
-		undoBtn.addActionListener(this);
-		pan.add(undoBtn);
-		redoBtn = new JButton("Redo");
-		redoBtn.addActionListener(this);
-		pan.add(redoBtn);
-		quitBtn = new JButton("Quit");
-		quitBtn.addActionListener(this);
-		pan.add(quitBtn);
-		add(pan, BorderLayout.NORTH);
-		canvas = new Canvas();
-		add(canvas, BorderLayout.CENTER);
-		pack();
-	}
-	
-	
-	public void actionPerformed(ActionEvent e)
-	{
-		if (e.getSource() == quitBtn)
-			System.exit(0);
-		else if (e.getSource() == undoBtn)
-			undo();
-		else if (e.getSource() == redoBtn)
-			redo();
-	}
-	
-	
-	void undo()
-	{
-		System.out.println("Undo not implemented");
-	}
+    public Painter() {
+        setTitle("Painter");
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	
-	void redo()
-	{
-		System.out.println("Redo not implemented");
-	}
-	
-	
-	Stack<Circle> getHistory()
-	{
-		return history;
-	}
+        history = new Stack<>();
+        undoHistory = new Stack<>();
 
-	
-	
-	Stack<Circle> getUndoHistory()
-	{
-		return undoHistory;
-	}
-	
-	
-	void setUndoButtonEnabled(boolean b)
-	{
-		undoBtn.setEnabled(b);
-	}
-	
-	
-	void setRedoButtonEnabled(boolean b)
-	{
-		redoBtn.setEnabled(b);
-	}
-	
-	
-	public static void main(String[] args)
-	{
-		new Painter().setVisible(true);
-	}
-	
+        // Top panel
+        JPanel topPanel = new JPanel();
+        colorChooser = new ColorChooser();
+        topPanel.add(colorChooser);
+
+        undoBtn = new JButton("Undo");
+        undoBtn.addActionListener(this);
+        undoBtn.setEnabled(false);
+        topPanel.add(undoBtn);
+
+        redoBtn = new JButton("Redo");
+        redoBtn.addActionListener(this);
+        redoBtn.setEnabled(false);
+        topPanel.add(redoBtn);
+
+        quitBtn = new JButton("Quit");
+        quitBtn.addActionListener(this);
+        topPanel.add(quitBtn);
+
+        add(topPanel, BorderLayout.NORTH);
+
+        // Canvas
+        canvas = new Canvas();
+        add(canvas, BorderLayout.CENTER);
+
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    // ----------------------------------------------------------------------
+    // Color Chooser Panel
+    // ----------------------------------------------------------------------
+
+    private class ColorChooser extends JPanel {
+        private JSlider[] sliders;
+        private SamplerPanel sampler;
+
+        ColorChooser() {
+            setLayout(new BorderLayout());
+            sampler = new SamplerPanel();
+
+            JPanel controls = new JPanel(new GridLayout(3, 1));
+            sliders = new JSlider[3];
+
+            for (int i = 0; i < 3; i++) {
+                JPanel row = new JPanel();
+                row.add(new JLabel("" + "RGB".charAt(i)));
+                sliders[i] = new JSlider(0, 255, i == 0 ? 255 : 0);
+                sliders[i].addChangeListener(sampler);
+                row.add(sliders[i]);
+                controls.add(row);
+            }
+
+            add(controls, BorderLayout.WEST);
+            add(sampler, BorderLayout.CENTER);
+        }
+
+        Color getColor() {
+            return new Color(
+                    sliders[0].getValue(),
+                    sliders[1].getValue(),
+                    sliders[2].getValue()
+            );
+        }
+
+        private class SamplerPanel extends JPanel implements ChangeListener {
+            public Dimension getPreferredSize() {
+                return new Dimension(50, 50);
+            }
+
+            public void stateChanged(ChangeEvent e) {
+                repaint();
+            }
+
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(getColor());
+                g.fillRect(0, 0, getWidth(), getHeight());
+            }
+        }
+    }
+
+    // ----------------------------------------------------------------------
+    // Canvas Panel
+    // ----------------------------------------------------------------------
+
+    private class Canvas extends JPanel implements MouseListener {
+        Canvas() {
+            addMouseListener(this);
+        }
+
+        public Dimension getPreferredSize() {
+            return new Dimension(500, 500);
+        }
+
+        public void mouseClicked(MouseEvent e) {
+            Circle circle = new Circle(e.getPoint(), 50, colorChooser.getColor());
+            history.push(circle);
+            undoHistory.clear();     // Reset redo history after new action
+            updateButtons();
+            repaint();
+        }
+
+        public void mousePressed(MouseEvent e) {}
+        public void mouseReleased(MouseEvent e) {}
+        public void mouseEntered(MouseEvent e) {}
+        public void mouseExited(MouseEvent e) {}
+
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            g.setColor(Color.WHITE);
+            g.fillRect(0, 0, getWidth(), getHeight());
+
+            for (Circle c : history)
+                c.paint(g);
+        }
+    }
+
+    // ----------------------------------------------------------------------
+    // Button Actions
+    // ----------------------------------------------------------------------
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == quitBtn) {
+            System.exit(0);
+        } else if (e.getSource() == undoBtn) {
+            undo();
+        } else if (e.getSource() == redoBtn) {
+            redo();
+        }
+    }
+
+    private void undo() {
+        if (!history.isEmpty()) {
+            undoHistory.push(history.pop());
+            updateButtons();
+            canvas.repaint();
+        }
+    }
+
+    private void redo() {
+        if (!undoHistory.isEmpty()) {
+            history.push(undoHistory.pop());
+            updateButtons();
+            canvas.repaint();
+        }
+    }
+
+    private void updateButtons() {
+        undoBtn.setEnabled(!history.isEmpty());
+        redoBtn.setEnabled(!undoHistory.isEmpty());
+    }
+
+    // ----------------------------------------------------------------------
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Painter().setVisible(true));
+    }
 }
